546f052f8c9b04182b2e030242837784289c4da2
untangh <hoppii007@gmail.com>
2024-01-29 09:44:04 +0900

    test: IteratorPattern

--- IteratorPattern/MainProgram.cs
+++ IteratorPattern/MainProgram.cs
@@ -7,6 +7,7 @@ namespace IteratorPattern{
 			BookShelf bookShelf = new BookShelf();
 			Iterator it = bookShelf.iterator();
 			bookShelf.AddBook("Java言語で学ぶデザインパターン");
+			bookShelf.AddBook("ベーシック圏論");
 			while(it.hasNext()){
 				Book book = (Book)it.next();
 				Console.WriteLine(book.name);
